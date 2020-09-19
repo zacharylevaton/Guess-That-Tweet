ab100a34e259f0f09e6024c711bac3ba7a1ecd9a
Zachary Levaton <53060403+zacharylevaton@users.noreply.github.com>
2020-09-19 16:53:44 -0700

    Delete keys.js

--- public/keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    consumer_key:         'OZMWhTZAdLmw15klBIaCSaYnV'
-  , consumer_secret:      'doaznagd95EhIfqwzL9Og55pJCjVya3XKP8kXCwOd1XR7t82pG'
-  , app_only_auth:        true
-};
