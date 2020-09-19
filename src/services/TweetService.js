7eb5ebffa4af7a7fee9d16ec23750fa57d2b898d
Zach Levaton <zach.levaton.0514@gmail.com>
2020-09-19 16:08:00 -0700

    Updating TweetServices to account for Heroku deployment.

--- src/services/TweetService.js
+++ src/services/TweetService.js
@@ -4,9 +4,10 @@ export default {
    async getTweets() {
       let running = true;
       while (running) {
-         let res = await axios.post("http://localhost:3000/");
+         let baseUrl = process.env.baseURL || "http://localhost:3000"
+         let res = await axios.post(baseUrl);
          while (!res.data) {
-            res = await axios.post("http://localhost:3000/");
+            res = await axios.post(baseUrl);
          }
          if (res.data.tweet.lang =="en" && !("retweeted_status" in res.data.tweet) && !res.data.tweet.in_reply_to_screen_name) {
             running = false
